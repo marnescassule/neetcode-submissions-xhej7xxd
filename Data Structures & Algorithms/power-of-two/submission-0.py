c3e16ae3e6de52d2149a38b00bc6270735937e6e
Marnes Cassule <marnes_cassule@hotmail.com>
2026-04-15 23:46:12 -0300

    Add: power-of-two - submission-0

---
+++ Data Structures & Algorithms/power-of-two/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        
+        if n == 1: 
+            return True
+
+        while(n>1):
+            n/=2
+
+        if(n == 1):
+            return True
+        else: 
+            return False
